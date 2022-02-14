91906dcbc62df1eb2aeb358b5fe56d3fe3e82c81
WrittenWoods <consideredlily@gmail.com>
2022-02-14 11:47:35 -0500

    diceRoller helper method
    Added helper method that parses string containing dice roll and returns numerical value

--- app/App.tsx
+++ app/App.tsx
@@ -1,15 +1,14 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { diceRoller } from './helpers/diceRoller';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
+          <p>{diceRoller("1d8")}</p>
         <a
           className="App-link"
           href="https://reactjs.org"
---
+++ app/helpers/diceRoller.ts
@@ -0,0 +1,22 @@
+export const diceRoller = function parseDiceRollAndReturnResult(str: string) {
+
+  let arr = str.split('')
+  let isDieType = false
+  let numArr = [], typeArr = []
+  let result = 0
+
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] === "d") {
+      isDieType = true
+    } else {
+    	isDieType ? typeArr.push(arr[i]) : numArr.push(arr[i])
+    }
+  }
+
+  let dieNumber = Number(numArr.join(''))
+  let dieType = Number(typeArr.join(''))
+  for (let i = 0; i < dieNumber; i++) { result += Math.ceil(Math.random() * dieType) }
+
+  return result
+
+}
