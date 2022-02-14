9fdcd44a50f8aa7010795def36b8e1efa2d627d2
WrittenWoods <consideredlily@gmail.com>
2022-02-14 13:52:19 -0500

    added function to return string with dice roll results interpolated

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,14 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import { diceRoller } from './helpers/diceRoller';
+import { interpolatedRoll } from './helpers/interpolatedRoll';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-          <p>{diceRoller("1d8")}</p>
+          <p>{interpolatedRoll("I deal 1d8 slashing damage, 2d6 fire damage")}</p>
         <a
           className="App-link"
           href="https://reactjs.org"
