316da01fc8ab9d014a64e8934e853589933789e9
WrittenWoods <consideredlily@gmail.com>
2022-04-14 13:35:35 -0400

    Added MDX

--- src/MenuItem.tsx
+++ src/MenuItem.tsx
@@ -137,7 +137,7 @@ function MenuItem ({ sheetState, newTitlePath }: MenuItemProps) {
   // returns content of the menu item component when editMode is toggled on
 
   function editModeContent() {
-    if (isMatch && contentType === 'text' || 'button') {
+    if (isMatch && typeof contentAtPath === 'string') {
       return (
         <span>
           <textarea value={title} onChange={(e) => setTitle(e.target.value)} />
